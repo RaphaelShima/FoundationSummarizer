89208b97ed4b661822ab93dc1bd73d372434c18c
Raphael Tavares Shimamoto <raphaelshimamoto@hotmail.com>
2026-09-13 21:05:24 -0300

    feature: Add file panel selection

--- FoundationSummarizer/ViewModel/SummarizerViewModel.swift
+++ FoundationSummarizer/ViewModel/SummarizerViewModel.swift
@@ -12,6 +12,9 @@ import UniformTypeIdentifiers
 
 protocol SummarizerViewModelProtocol {
     var itens: [SummaryItem] { get }
+    
+    func openFilesPanel() async
+    func handleFileSelection(url: URL) async -> String
 }
 
 @Observable
@@ -32,8 +35,30 @@ final class SummarizerViewModel: SummarizerViewModelProtocol {
         print("Resumir")
     }
     
-    func openFinder() {
+    func openFilesPanel() async {
         let panel = NSOpenPanel()
+        panel.canChooseFiles = true
         panel.allowedContentTypes = [.text, .pdf, .swiftSource]
+        panel.prompt = "Choose"
+        
+        if panel.runModal() == .OK, let url = panel.url {
+            let content = await handleFileSelection(url: url)
+        }
+    }
+    
+    func handleFileSelection(url: URL) async -> String {
+        guard url.startAccessingSecurityScopedResource() else {
+            print("No permission to access file: \(url)")
+            return ""
+        }
+        do { url.stopAccessingSecurityScopedResource() }
+        
+        do {
+            return try String(contentsOf: url,
+                                 encoding: .utf8)
+        } catch {
+            print("Faile to read file \(url): \(error)")
+            return ""
+        }
     }
 }
